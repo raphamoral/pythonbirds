bec69259073835cecab4148015614e314d6e4938
Raphael Moral Piazera <raphamoraleng@gmail.com>
2021-09-15 00:49:53 -0300

    Criandos diferentes tipos de  Atributos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos =2
     def __init__(self,*filhos,nome= None,idade=35):
         self.idade = idade
 
@@ -13,8 +14,14 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
+    luciano.olhos=1
+    del luciano.olhos
+    del luciano.filhos
     print (luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome ='Ramalho'
+    print(renzo.__dict__)
+    print(luciano.__dict__)
 
